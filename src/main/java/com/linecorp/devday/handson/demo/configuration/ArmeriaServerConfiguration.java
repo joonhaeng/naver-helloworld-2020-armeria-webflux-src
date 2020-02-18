db28d4f04c0488eb25cac6e8701e03830f5f5f18
Joonhaeng Lee <joonhaeng.lee@navercorp.com>
2020-02-18 15:39:10 +0900

    fix for breaking changes(preview)

--- src/main/java/com/linecorp/devday/handson/demo/configuration/ArmeriaServerConfiguration.java
+++ src/main/java/com/linecorp/devday/handson/demo/configuration/ArmeriaServerConfiguration.java
@@ -1,40 +1,45 @@
 package com.linecorp.devday.handson.demo.configuration;
 
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.linecorp.armeria.common.grpc.GrpcSerializationFormats;
 import com.linecorp.armeria.server.docs.DocService;
 import com.linecorp.armeria.server.grpc.GrpcService;
 import com.linecorp.armeria.server.logging.AccessLogWriter;
+import com.linecorp.armeria.server.logging.ContentPreviewingService;
 import com.linecorp.armeria.server.logging.LoggingService;
 import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
 import com.linecorp.devday.handson.demo.service.DemoGrpcService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class ArmeriaServerConfiguration {
 
-    private final DemoGrpcService demoGrpcService;
+	private final DemoGrpcService demoGrpcService;
 
-    @Bean
-    public ArmeriaServerConfigurator armeriaServerConfigurator() {
+	@Bean
+	public ArmeriaServerConfigurator armeriaServerConfigurator() {
 
-        return serverBuilder -> {
+		return serverBuilder -> {
 
-            serverBuilder.decorator(LoggingService.newDecorator());
-            serverBuilder.accessLogWriter(AccessLogWriter.combined(), false);
+			serverBuilder.decorator(LoggingService.newDecorator());
+			serverBuilder.decorator(ContentPreviewingService.newDecorator(Integer.MAX_VALUE, StandardCharsets.UTF_8));
+			serverBuilder.accessLogWriter(AccessLogWriter.combined(), false);
 
-            serverBuilder.service(GrpcService.builder()
-                    .addService(demoGrpcService)
-                    .supportedSerializationFormats(GrpcSerializationFormats.values())
-                    .enableUnframedRequests(true)
-                    .build());
+			serverBuilder.service(GrpcService.builder()
+				.addService(demoGrpcService)
+				.supportedSerializationFormats(GrpcSerializationFormats.values())
+				.enableUnframedRequests(true)
+				.build());
 
-            serverBuilder.serviceUnder("/docs", new DocService());
+			serverBuilder.serviceUnder("/docs", new DocService());
 
-        };
+		};
 
-    }
+	}
 
 }
