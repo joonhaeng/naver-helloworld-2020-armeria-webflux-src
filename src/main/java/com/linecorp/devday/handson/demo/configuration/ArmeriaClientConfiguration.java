1a26a7c15bf629436c13e78792d1a5ad27760b39
Joonhaeng Lee <joonhaeng.lee@navercorp.com>
2020-02-18 15:38:41 +0900

    fix for breaking changes(tls, preview)

--- src/main/java/com/linecorp/devday/handson/demo/configuration/ArmeriaClientConfiguration.java
+++ src/main/java/com/linecorp/devday/handson/demo/configuration/ArmeriaClientConfiguration.java
@@ -1,52 +1,52 @@
 package com.linecorp.devday.handson.demo.configuration;
 
-import com.linecorp.armeria.client.ClientFactory;
-import com.linecorp.armeria.client.logging.LoggingClient;
-import com.linecorp.armeria.spring.web.reactive.ArmeriaClientConfigurator;
-import com.linecorp.devday.handson.demo.decorator.PrettyJsonPreviewClientDecorator;
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.nio.charset.StandardCharsets;
+import com.linecorp.armeria.client.ClientFactory;
+import com.linecorp.armeria.client.logging.ContentPreviewingClient;
+import com.linecorp.armeria.client.logging.LoggingClient;
+import com.linecorp.armeria.spring.web.reactive.ArmeriaClientConfigurator;
+import com.linecorp.devday.handson.demo.decorator.PrettyJsonPreviewClientDecorator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class ArmeriaClientConfiguration {
 
-    private final PrettyJsonPreviewClientDecorator prettyJsonPreviewClientDecorator;
+	private final PrettyJsonPreviewClientDecorator prettyJsonPreviewClientDecorator;
 
-    @Bean
-    public ClientFactory clientFactory() {
+	@Bean
+	public ClientFactory clientFactory() {
 
-        return ClientFactory.builder().sslContextCustomizer(
-                b -> b.trustManager(InsecureTrustManagerFactory.INSTANCE)).build();
+		return ClientFactory.builder().tlsNoVerify().build();
 
-    }
+	}
 
-    @Bean
-    public ArmeriaClientConfigurator armeriaClientConfigurator(ClientFactory clientFactory) {
+	@Bean
+	public ArmeriaClientConfigurator armeriaClientConfigurator(ClientFactory clientFactory) {
 
-        return clientBuilder -> {
+		return clientBuilder -> {
 
-            clientBuilder.decorator(LoggingClient.newDecorator());
-            clientBuilder.contentPreview(500, StandardCharsets.UTF_8);
-            clientBuilder.decorator(prettyJsonPreviewClientDecorator);
-            clientBuilder.factory(clientFactory);
+			clientBuilder.decorator(LoggingClient.newDecorator());
+			clientBuilder.decorator(ContentPreviewingClient.newDecorator(Integer.MAX_VALUE, StandardCharsets.UTF_8));
+			clientBuilder.decorator(prettyJsonPreviewClientDecorator);
+			clientBuilder.factory(clientFactory);
 
-        };
+		};
 
-    }
+	}
 
-    @Bean
-    public WebClient webClient(WebClient.Builder armeriaWebClientBuilder) {
+	@Bean
+	public WebClient webClient(WebClient.Builder armeriaWebClientBuilder) {
 
-        return armeriaWebClientBuilder.build();
+		return armeriaWebClientBuilder.build();
 
-    }
+	}
 
 }
